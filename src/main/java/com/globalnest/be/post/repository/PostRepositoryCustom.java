cf0a4aba583a8d2a246c145ea3c0ee4d10d676d0
leedy3838 <leedy3838@naver.com>
2024-11-23 19:03:05 +0900

    feat: 필터링 기능 구현

--- src/main/java/com/globalnest/be/post/repository/PostRepositoryCustom.java
+++ src/main/java/com/globalnest/be/post/repository/PostRepositoryCustom.java
@@ -2,10 +2,11 @@ package com.globalnest.be.post.repository;
 
 import com.globalnest.be.post.application.type.SortType;
 import com.globalnest.be.post.repository.dto.PostRepoResponse;
+import com.globalnest.be.user.domain.type.Part;
 import java.util.List;
 
 public interface PostRepositoryCustom {
 
-    List<PostRepoResponse> findPostResponseList(Long userId, int page, int size, SortType sortType);
+    List<PostRepoResponse> findPostResponseList(Long userId, int page, int size, SortType sortType, Part part);
     PostRepoResponse findPostDetailResponse(Long userId, Long postId);
 }
