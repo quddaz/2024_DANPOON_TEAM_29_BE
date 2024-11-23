3a9dd86dc379c8079353dcc4a889a4a0cd6002dc
leedy3838 <leedy3838@naver.com>
2024-11-24 03:45:13 +0900

    feat: 게시글, 태그 더미 데이터 추가

--- src/main/java/com/globalnest/be/post/repository/init/PostTagInitializer.java
+++ src/main/java/com/globalnest/be/post/repository/init/PostTagInitializer.java
@@ -6,7 +6,10 @@ import com.globalnest.be.post.domain.PostTag;
 import com.globalnest.be.post.repository.PostRepository;
 import com.globalnest.be.post.repository.PostTagRepository;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Random;
+import java.util.Set;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.ApplicationArguments;
@@ -22,28 +25,37 @@ public class PostTagInitializer implements ApplicationRunner {
     private final PostTagRepository postTagRepository;
     private final PostRepository postRepository;
 
+    private final Random random = new Random();
+    private final String[] tags = {"ANGRY", "HAPPY", "SAD", "EXCITED", "BORED", "SURPRISED"};
+
     @Override
     public void run(ApplicationArguments args) {
         if (postTagRepository.count() > 0) {
-            log.info("[PostTag]더미 데이터 존재");
+            log.info("[PostTag] 더미 데이터 존재");
         } else {
-            Post DUMMY_POST1 = postRepository.findById(1L).orElseThrow();
-
             List<PostTag> postTagList = new ArrayList<>();
 
-            PostTag postTag1 = PostTag.builder()
-                    .post(DUMMY_POST1)
-                    .tag("ANGRY")
-                    .build();
-            PostTag postTag2 = PostTag.builder()
-                    .post(DUMMY_POST1)
-                    .tag("HAPPY")
-                    .build();
+            for (long postId = 1; postId <= 24; postId++) {
+                Post post = postRepository.findById(postId).orElseThrow();
 
-            postTagList.add(postTag1);
-            postTagList.add(postTag2);
+                Set<String> usedTags = new HashSet<>(); // 중복 태그 방지를 위한 Set
+
+                // 랜덤하게 태그를 선택하여 생성
+                while (usedTags.size() < 3) { // 각 포스트에 3개의 태그를 달기
+                    String randomTag = tags[random.nextInt(tags.length)];
+                    if (!usedTags.contains(randomTag)) { // 중복 체크
+                        PostTag postTag = PostTag.builder()
+                                .post(post)
+                                .tag(randomTag)
+                                .build();
+                        postTagList.add(postTag);
+                        usedTags.add(randomTag); // 사용한 태그 추가
+                    }
+                }
+            }
 
             postTagRepository.saveAll(postTagList);
+            log.info("[PostTag] 더미 데이터 초기화 완료");
         }
     }
 }
